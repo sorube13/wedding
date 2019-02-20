79130581cf295121338585d5cce2c323ca8fa2b2
sorube13 <sorube@gmail.com>
2019-02-20 19:02:15 +0100

    add timestamp to logs

--- app.js
+++ app.js
@@ -1,66 +1,80 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+require('console-stamp')(console, 'yyyy-mm-dd HH:MM:ss')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.all('/*', handleRedirect);
+app.all('/*', handleRedirect)
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
 
-
-function handleRedirect(req, res, next) {
-    if (req.headers && req.headers.host && req.headers.host.match(/^www/) !== null) {
-        res.redirect(req.protocol + '://' + req.headers.host.replace(/^www\./, '') + req.url);
-    } else {
-        next();
-    }
+function handleRedirect (req, res, next) {
+  console.log('Host: ' + req.headers ? req.headers.host : 'No hosts')
+  if (
+    req.headers &&
+    req.headers.host &&
+    req.headers.host.match(/^www/) !== null
+  ) {
+    console.log(
+      'Redirect URL' +
+        req.protocol +
+        '://' +
+        req.headers.host.replace(/^www\./, '') +
+        req.url
+    )
+    res.redirect(
+      req.protocol + '://' + req.headers.host.replace(/^www\./, '') + req.url
+    )
+  } else {
+    next()
+  }
 }
 
-module.exports = app;
+module.exports = app
